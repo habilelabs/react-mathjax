a7d10565bc17e9c85e8e6c32131e1fd1f4dd2434
Shankar Morwal <shankar@habilelabs.io>
2019-05-24 13:57:47 +0530

    bug fix.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Node from './Node'
 import Context from './Context'
 import Text from './Text'
+import Html from './Html'
 
-export { Node, Context, Text }
-export default { Node, Context, Text }
+export { Node, Context, Text, Html }
+export default { Node, Context, Text, Html }
